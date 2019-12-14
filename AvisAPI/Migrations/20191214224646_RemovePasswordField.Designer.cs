c2516ecb860e2f46ec5b5918eb22ff23bece3ef4
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2019-12-14 23:48:16 +0100

    Removed password field from AppUser entity

--- AvisAPI/Migrations/20191214224646_RemovePasswordField.Designer.cs
+++ AvisAPI/Migrations/20191214224646_RemovePasswordField.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using AvisAPI.DbContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace AvisAPI.Migrations
 {
     [DbContext(typeof(AvisDbContext))]
-    partial class AvisDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191214224646_RemovePasswordField")]
+    partial class RemovePasswordField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AvisAPI/Migrations/20191214224646_RemovePasswordField.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AvisAPI.Migrations
+{
+    public partial class RemovePasswordField : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
